fee167c3f249618eead9e389edf93e62a7f1baba
esrig <joshua.esrig@gmail.com>
2022-07-12 23:01:12 -0700

    player class created, with a get_move method that will reject a column that isn't between ranges 1-7 (inclusive)

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -14,7 +14,7 @@ class Board
         @gameboard.each do |array|
             print "|"
             array.each do |symbol|
-                print "  " if symbol == " "
+                print "   " if symbol == " "
             end
             print "|"
             puts   
@@ -23,6 +23,24 @@ class Board
     attr_reader :gameboard
 end
 
+class Player
+    def initialize(sym)
+        @sym = sym
+    end
+
+    def get_move
+        puts "Enter in a column to make your move in"
+        column = gets.chomp.to_i
+        while column > 7 || column < 1 do
+            puts "Your move wasn't a valid column, enter a valid column"
+            column = gets.chomp.to_i
+        end
+        puts column
+    end
+end
+
 board = Board.new
 board.init_new_gameboard
 board.print_game
+player1 = Player.new("x")
+player1.get_move
