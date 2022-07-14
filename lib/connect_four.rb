d82b784d9be131c9c7646f1ef5cc49583339410f
esrig <joshua.esrig@gmail.com>
2022-07-13 20:04:07 -0700

    finalized tests for the Board#check_winner? method, tests in all possible winning directions

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,6 +3,10 @@ class Board
     def init_new_gameboard
         @gameboard = Array.new(6) { Array.new(7, " ") }
     end
+    #for use by tester file
+    def change_gameboard(i, j, symbol)
+        @gameboard[i][j] = symbol
+    end
 
     def print_game
         puts
@@ -27,14 +31,28 @@ class Board
     end
 
     def check_winner?
-        (1..6).each do |i|
-            (1..7).each do |j|
-                #0pi win(unit circle)
-                continue if(j > 4)
-                if @gameboard[i][j] == @gameboard[i][j+1] 
+        (0..5).each do |i|
+            (0..6).each do |j|
+                #check if 4 of the same pieces are horizontal from eachother
+                if(j < 5) 
+                    check_arr = [@gameboard[i][j], @gameboard[i][j+1], @gameboard[i][j+2], @gameboard[i][j+3]]
+                    #print check_arr
+                    #print check_arr[0] == " "
+                    #puts check_arr.uniq.length
+                    if check_arr.uniq.length == 1 && check_arr[0] != " "
+                        return true
+                    end
+                end
+                #check if 4 pieces of the same peices are diagonal with a positive slop (pi/4 on unit circle)
+                if(j < 5 && i > 2)
+                    check_arr = [@gameboard[i][j], @gameboard[i-1][j+1], @gameboard[i-2][j+2], @gameboard[i-3][j+3]]
+                    if check_arr.uniq.length == 1 && check_arr[0] != " "
+                        return true
+                    end
                 end
             end
         end
+        return false
     end
 
     def place_move(col, symbol)
@@ -49,7 +67,7 @@ class Board
         end
     end
 
-    attr_reader :gameboard
+    attr_accessor :gameboard
 end
 
 class Player
@@ -67,7 +85,35 @@ class Player
         return column
     end
 
-    
+    attr_reader :sym
 end
+
+def execute_game(board, p1, p2)
+    board.print_game
+    while(true)
+        
+        board.place_move(p1.get_move, p1.sym)
+        board.print_game
+        if(board.check_winner?)
+            puts "player 1 wins!"
+            break
+        end
+        
+        board.place_move(p2.get_move, p2.sym)
+        board.print_game
+        if(board.check_winner?)
+            puts "player 2 wins!"
+            break
+        end
+    end
+end
+=begin
+p1 = Player.new("x")
+p2 = Player.new("o")
+board = Board.new
+board.init_new_gameboard
+execute_game(board, p1, p2)
+    
+=end
 
 
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -26,4 +26,47 @@ describe Board do
             }.to raise_error("Column is already full")
         end
     end
+
+    describe "#check_winner?" do
+        board = Board.new
+        board.init_new_gameboard
+        it "declares a winner when 4 of the same gamepieces are horizontal" do
+            board.init_new_gameboard
+            board.place_move(2, "x")
+            board.place_move(3, "x")
+            board.place_move(4, "x")
+            board.place_move(5, "x")
+            expect(board.check_winner?).to eql(true)
+        end
+
+        it "declares a winner when 4 of the same gamepieces are vertical" do
+            board.init_new_gameboard
+            board.place_move(2, "x")
+            board.place_move(2, "x")
+            board.place_move(2, "x")
+            board.place_move(2, "x")
+            expect(board.check_winner?).to eql(true)
+        end
+        
+        it "declares a winner when 4 of the same gamepieces are diagonal with a positive slope" do
+            board.init_new_gameboard
+            board.change_gameboard(5, 0, "x")
+            board.change_gameboard(4, 1, "x")
+            board.change_gameboard(3, 2, "x")
+            board.change_gameboard(2, 3, "x")
+            board.print_game
+            expect(board.check_winner?).to eql(true)
+        end
+        it "declares a winner when 4 of the same gamepieces are diagonal with a negative slope" do
+            board.init_new_gameboard
+            board.change_gameboard(5, 3, "x")
+            board.change_gameboard(4, 2, "x")
+            board.change_gameboard(3, 1, "x")
+            board.change_gameboard(2, 0, "x")
+            expect(board.check_winner?).to eql(true)
+        end
+
+
+    end
+
 end
